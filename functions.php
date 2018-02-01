7196b51abb749061950274ee04e40cd1d7f1dc10
Chris Baldelomar <chris@webplantmedia.com>
2018-02-01 15:40:39 -0600

    update

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function crimson_rose_child_theme_child_enqueue_parent_theme_style() {
+function crimson_rose_child_enqueue_parent_theme_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'crimson_rose_child_theme_child_enqueue_parent_theme_style' );
+add_action( 'wp_enqueue_scripts', 'crimson_rose_child_enqueue_parent_theme_style' );
