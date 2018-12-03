0a5a983bc54247b26ed6e6c4dfef452144331d29
Chris Baldelomar <chris@webplantmedia.com>
2018-12-03 14:05:50 -0600

    name change

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function painted_lady_child_enqueue_parent_theme_style() {
+function adonis_blue_child_enqueue_parent_theme_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'painted_lady_child_enqueue_parent_theme_style' );
+add_action( 'wp_enqueue_scripts', 'adonis_blue_child_enqueue_parent_theme_style' );
